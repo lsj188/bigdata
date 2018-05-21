859f841b46885d8ce72e94492e79768b61b9e7bc
luoshijun <luoshijun58@163.com>
2018-05-21 17:03:03 +0800

    update

--- kafka/src/main/scala/cn/lsj/demo/KafkaEventProducer.scala
+++ kafka/src/main/scala/cn/lsj/demo/KafkaEventProducer.scala
@@ -2,14 +2,36 @@ package cn.lsj.demo
 
 import java.util.Properties
 
-import kafka.javaapi.producer.Producer
-import kafka.producer.{KeyedMessage, ProducerConfig}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.json.JSONObject
 
 import scala.util.Random
 
 
 object KafkaEventProducer {
+    def getKafkaProdecer(brokerList:String): KafkaProducer[String, String] = {
+        val props = new Properties()
+        props.put("client.id", "ScalaProducerExample")
+        props.put("metadata.broker.list", brokerList)
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList) //格式：host1:port1,host2:port2,....
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, new Integer(0)) //a batch size of zero will disable batching entirely
+        props.put(ProducerConfig.LINGER_MS_CONFIG, new Integer(0)) //send message without delay
+        props.put(ProducerConfig.ACKS_CONFIG, "1") //对应partition的leader写到本地后即返回成功。极端情况下，可能导致失败
+        props.put("serializer.class", "kafka.serializer.StringEncoder")
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        new KafkaProducer[String, String](props)
+    }
+
+    def send2Kafka(kafkaProducer: KafkaProducer[String, String], topic: String, lines: List[String]) = {
+        for ( line <- lines){
+            val record = new ProducerRecord[String,String](topic, line)
+            println(record)
+            kafkaProducer.send(record).get()
+        }
+
+    }
+
 
     private val users = Array(
         "4A4D769EB9679C054DE81B973ED5D768", "8dfeb5aaafc027d89349ac9a20b3930f",
@@ -17,7 +39,7 @@ object KafkaEventProducer {
         "068b746ed4620d25e26055a9f804385f", "97edfc08311c70143401745a03a50706",
         "d7f141563005d1b5d0d3dd30138f3f62", "c8ee90aade1671a21336c721512b817a",
         "6b67c8c700427dee7552f81f3228c927", "a95f22eabc4fd4b580c011a3161a9d9d")
-//    private val users = Array("97edfc08311c70143401745a03a50706")
+    //    private val users = Array("97edfc08311c70143401745a03a50706")
 
     private val random = new Random()
     private var pointer = -1
@@ -41,35 +63,26 @@ object KafkaEventProducer {
     // bin/kafka-topics.sh --zookeeper zk1:2181,zk2:2181,zk3:2181/kafka --describe user_events
     // bin/kafka-console-consumer.sh --zookeeper zk1:2181,zk2:2181,zk3:22181/kafka --topic test_json_basis_event --from-beginning
     def main(args: Array[String]): Unit = {
-//        val topic = "user_events"
-        val topic = "streams-plaintext-input"
+        val topic = "user_events"
+        //        val topic = "streams-plaintext-input"
         val brokers = "127.0.0.1:9092"
-        val props = new Properties()
-        props.put("metadata.broker.list", brokers)
-        props.put("serializer.class", "kafka.serializer.StringEncoder")
-        val kafkaConfig = new ProducerConfig(props)
-        val producer = new Producer[String, String](kafkaConfig)
-
+        val producer = getKafkaProdecer(brokers)
 
         while (true) {
 
             // prepare event data
             val event = new JSONObject()
             event
-              .put("uid", getUserID)
-              .put("event_time", System.currentTimeMillis.toString)
-              .put("os_type", "Android")
-              .put("click_count", click)
+                .put("uid", getUserID)
+                .put("event_time", System.currentTimeMillis.toString)
+                .put("os_type", "Android")
+                .put("click_count", click)
 
 
-            val line=getUserID+" "+" "+click+" "+System.currentTimeMillis.toString
+//            val line = getUserID + " " + " " + click + " " + System.currentTimeMillis.toString
+
             // produce event message
-
-//            producer.send(new KeyedMessage[String, String](topic, event.toString))
-            producer.send(new KeyedMessage[String, String](topic,line ))
-            println("Message sent: " + line)
-
-
+            send2Kafka(producer, topic, List[String](event.toString()))
             Thread.sleep(2000)
 
         }
--- kafka/src/main/scala/cn/lsj/demo/KafkaStreamsCustPip.scala
+++ kafka/src/main/scala/cn/lsj/demo/KafkaStreamsCustPip.scala
@@ -5,11 +5,8 @@ import java.util.concurrent.TimeUnit
 
 import org.apache.kafka.common.serialization._
 import org.apache.kafka.streams._
-import org.apache.kafka.streams.kstream.{KStream, KTable, Produced}
+import org.apache.kafka.streams.kstream._
 import org.codehaus.jettison.json.JSONObject
-
-import scala.collection.JavaConverters.asJavaIterableConverter
-
 
 object KafkaStreamsCustPip {
     def main(args: Array[String]) {
@@ -18,26 +15,28 @@ object KafkaStreamsCustPip {
             val p = new Properties()
             p.put(StreamsConfig.APPLICATION_ID_CONFIG, "user_events_pip_app")
             p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
-            p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-            p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass)
+            p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
+            p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
             p
         }
 
         val builder: StreamsBuilder = new StreamsBuilder()
         val textLines: KStream[String, String] = builder.stream("user_events")
-//        val wordCounts: KTable[String, Integer] = textLines.map { (k: String, v: String) => {
-//            val json:JSONObject=null
-//            try {
-//                val json = new JSONObject(v)
-//            }
-//            catch {
-//                case e: Exception => e.printStackTrace()
-//            }
-//            KeyValue.pair(json.getString("uid"), json.getInt("click_count"))
-//        }
-//        }.groupByKey().reduce(_ + _)
         textLines.print()
-//        wordCounts.toStream().to("user_events_pip", Produced.`with`(Serdes.String(), Serdes.Integer()))
+        val wordCounts: KTable[String, Long] = textLines.map[String, Long] {
+            //kafka有些不支持scala语法，需要按JAVA来写，注意看编译错误信息
+            new KeyValueMapper[String, String, KeyValue[String, Long]] {
+                override def apply(k: String, v: String): KeyValue[String, Long] = {
+                    val json: JSONObject = new JSONObject(v)
+                    return KeyValue.pair(json.getString("uid"), json.getLong("click_count"))
+                }
+            }
+        }.groupByKey().reduce(new Reducer[Long] {
+            //kafka有些不支持scala语法，需要按JAVA来写，注意看编译错误信息
+            override def apply(aggValue: Long, newValue: Long): Long = aggValue + newValue
+        })
+
+        wordCounts.toStream().to("user_events_pip")
 
         val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
         streams.start()
