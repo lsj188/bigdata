d4f71c44ea33abdebd3844cad6122eb3bc7ab2bd
dev.wang <dev.wang@pingan.com.cn>
2019-01-23 17:03:44 +0800

    update

--- kafka/src/main/scala/cn/lsj/demo/KafkaCustClient.scala
+++ kafka/src/main/scala/cn/lsj/demo/KafkaCustClient.scala
@@ -1,5 +1,35 @@
 package cn.lsj.demo
 
-class KafkaCustClient {
+import java.util.Properties
+import java.util.concurrent.TimeUnit
 
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
+import org.apache.kafka.streams.kstream.KStream
+
+object KafkaCustClient {
+    def main(args: Array[String]) {
+
+        val config: Properties = {
+            val p = new Properties()
+            p.put(StreamsConfig.APPLICATION_ID_CONFIG, "user_events_pip_cust1")
+            p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+            p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
+            p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
+            p.put("group.id","pip_cust1")
+            p
+        }
+
+        val builder: StreamsBuilder = new StreamsBuilder()
+        val user_events_pip: KStream[String, String] = builder.stream("user_events_pip")
+        user_events_pip.print()
+
+        val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
+        streams.start()
+
+        Runtime.getRuntime.addShutdownHook(new Thread { () => {
+            streams.close(10, TimeUnit.SECONDS)
+        }
+        })
+    }
 }
--- kafka/src/main/scala/cn/lsj/demo/KafkaCustClient1.scala
+++ kafka/src/main/scala/cn/lsj/demo/KafkaCustClient1.scala
@@ -4,18 +4,19 @@ import java.util.Properties
 import java.util.concurrent.TimeUnit
 
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
 import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
 
-object KafkaCustClient {
+object KafkaCustClient1 {
     def main(args: Array[String]) {
 
         val config: Properties = {
             val p = new Properties()
-            p.put(StreamsConfig.APPLICATION_ID_CONFIG, "user_events_pip_cust1")
+            p.put(StreamsConfig.APPLICATION_ID_CONFIG, "user_events_pip_cust2")
             p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
             p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
             p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass.getName)
+            p.put("group.id","pip_cust1")
             p
         }
 
